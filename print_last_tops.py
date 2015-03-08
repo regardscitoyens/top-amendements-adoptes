a66d200e9d6e3d819de34699c6eaab446142b247
RouxRC <b.ooghe@gmail.com>
2015-03-08 22:36:29 +0100

    order top

--- print_last_tops.py
+++ print_last_tops.py
@@ -2,18 +2,24 @@
 # -*- coding: utf-8 -*-
 
 import codecs
-
+import re
 
 def unicode_print(s):
     print s.encode("utf-8")
 
+re_adoptes = re.compile(r'\((\d+) adopt')
+get_adoptes = lambda x: int(re_adoptes.search(x).group(1)) if x else 0
 
 def get_last_tops(typeparls):
     with codecs.open("data/%s-tops.csv" % typeparls, encoding="utf-8") as f:
         rows = f.readlines()
-        headers = rows[0].split(',')
+        headers = rows[0].strip('\n').split(',')
         tops = dict((header.strip(), value.strip()) for header, value in zip(headers, rows[-1].split(',')))
-        tops["groupes"] = [header.strip() for header in headers if header not in [u"mois", u"année"]]
+        tops["groupes"] = [v for v, _ in sorted(
+          [(header, get_adoptes(tops[header]))
+            for header in headers
+            if header not in [u"mois", u"année"]
+          ], key=lambda x: -x[1])]
         return tops
 
 top_deputes = get_last_tops("deputes")
